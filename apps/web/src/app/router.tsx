f91bf400ca3bdf6de31004284ee62fe77f426307
Guy Watson <guy@gwatson.com>
2026-04-08 06:29:18 +0700

    Wire router to KeyholdingPage component

--- apps/web/src/app/router.tsx
+++ apps/web/src/app/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { BrowsePage } from '../pages/BrowsePage';
 import { HomePage } from '../pages/HomePage';
 import { NotFoundPage } from '../pages/NotFoundPage';
-import { Keyholding } from '../pages/Keyholding';
+import { KeyholdingPage } from '../pages/KeyholdingPage';
 import { TasksPage } from '../pages/TasksPage';
 
 export const router = createBrowserRouter([
@@ -11,6 +11,6 @@ export const router = createBrowserRouter([
   { path: '/clips/:clipId', element: <BrowsePage /> },
   { path: '/tasks', element: <TasksPage /> },
   { path: '/tasks/:tierId', element: <TasksPage /> },
-  { path: '/keyholding', element: <Keyholding /> },
+  { path: '/keyholding', element: <KeyholdingPage /> },
   { path: '*', element: <NotFoundPage /> },
 ]);
