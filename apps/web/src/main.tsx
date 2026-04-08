a003921210aeeadd06996f7cacd1b8435968a7d1
Guy Watson <guy@gwatson.com>
2026-04-09 04:45:26 +0700

    Allow vertical pan over tier carousels on keyholding

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css?v=20260409i';
+import './styles.css?v=20260409j';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
